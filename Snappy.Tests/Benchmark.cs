b1857ed41d4f954036ca0c432f6053355d33c85f
Robert Vazan <robvcode@gmail.com>
2014-02-05 18:49:15 +0100

    Benchmarks

---
+++ Snappy.Tests/Benchmark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snappy.Tests
+{
+    public class Benchmark
+    {
+        public static readonly string DataPath = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath), "..", "..", "..", "testdata");
+        public int Iterations { get; private set; }
+        public byte[] Input { get; private set; }
+        public string Note { get; set; }
+        public readonly Stopwatch Stopwatch = new Stopwatch();
+
+        public static string[] GetTestFiles()
+        {
+            return Directory.GetFiles(DataPath).Select(Path.GetFileNameWithoutExtension).ToArray();
+        }
+
+        public static void Run(string type, string file, Action<Benchmark> action)
+        {
+            var data = File.ReadAllBytes(Directory.GetFiles(DataPath, file + ".*")[0]);
+            Benchmark last = null;
+            for (int iterations = 1; last == null || last.Iterations < 1000000000 && last.Stopwatch.Elapsed < TimeSpan.FromMilliseconds(500); iterations *= 2)
+            {
+                var benchmark = new Benchmark();
+                benchmark.Iterations = iterations;
+                benchmark.Input = data;
+                benchmark.Note = "";
+                action(benchmark);
+                last = benchmark;
+            }
+            var speed = last.Iterations * (double)data.Length / 1024 / 1024;
+            if (speed >= 1000)
+                Console.WriteLine("{0} {1}: {2:0.0} GB/s{3}", type, file, speed / 1024, last.Note);
+            else
+                Console.WriteLine("{0} {1}: {2:0} MB/s{3}", type, file, speed, last.Note);
+        }
+    }
+}
---
+++ Snappy.Tests/CodecThroughputTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snappy.Tests
+{
+    [TestFixture]
+    public class CodecThroughputTest
+    {
+        [Test, TestCaseSource(typeof(Benchmark), "GetTestFiles")]
+        public void Compression(string name)
+        {
+            Benchmark.Run("Compressing", name, benchmark =>
+            {
+                var output = new byte[SnappyCodec.GetMaxCompressedLength(benchmark.Input.Length)];
+                int length = 0;
+                benchmark.Stopwatch.Start();
+                for (int i = 0; i < benchmark.Iterations; ++i)
+                    length = SnappyCodec.Compress(benchmark.Input, 0, benchmark.Input.Length, output, 0);
+                benchmark.Stopwatch.Stop();
+                var roundtrip = new byte[benchmark.Input.Length];
+                var roundtripLength = SnappyCodec.Uncompress(output, 0, length, roundtrip, 0);
+                CollectionAssert.AreEqual(benchmark.Input, roundtrip.Take(roundtripLength));
+                benchmark.Note = String.Format(" ({0:0.00 %})", length / (double)benchmark.Input.Length);
+            });
+        }
+
+        [Test, TestCaseSource(typeof(Benchmark), "GetTestFiles")]
+        public void Uncompression(string name)
+        {
+            Benchmark.Run("Uncompressing", name, benchmark =>
+            {
+                var compressed = SnappyCodec.Compress(benchmark.Input);
+                var roundtrip = new byte[benchmark.Input.Length];
+                int length = 0;
+                benchmark.Stopwatch.Start();
+                for (int i = 0; i < benchmark.Iterations; ++i)
+                    length = SnappyCodec.Uncompress(compressed, 0, compressed.Length, roundtrip, 0);
+                benchmark.Stopwatch.Stop();
+                CollectionAssert.AreEqual(benchmark.Input, roundtrip);
+            });
+        }
+
+        [Test, TestCaseSource(typeof(Benchmark), "GetTestFiles")]
+        public void Validation(string name)
+        {
+            Benchmark.Run("Validating", name, benchmark =>
+            {
+                var compressed = SnappyCodec.Compress(benchmark.Input);
+                bool ok = false;
+                benchmark.Stopwatch.Start();
+                for (int i = 0; i < benchmark.Iterations; ++i)
+                    ok = SnappyCodec.Validate(compressed);
+                benchmark.Stopwatch.Stop();
+                Assert.IsTrue(ok);
+            });
+        }
+    }
+}
